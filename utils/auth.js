64ef00fb8c248d3f8e78e6ad7556446e0c04a67b
jacsel <bjacsel@yahoo.com>
2024-11-14 19:57:38 -0500

    logout button is now functional

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,7 @@
 const withAuth = (req, res, next) => {
-    // If user is not logged in, redirect to login page
     if (!req.session.logged_in) {
         res.redirect('/login');
     } else {
-        // If user is logged in, proceed to the requested page
         next();
     }
 };
