08b7afe8510c68b6e95b0e57914bcdd21ef98d2a
Alberto C. Garcia <albertogarcia767@yahoo.com>
2024-11-19 18:30:37 -0500

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,33 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Create connection to database
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // Production database connection
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  // Local database connection
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-        logging: false
+      host: 'localhost',
+      dialect: 'postgres',
+      port: 5432,
+      logging: false
     }
-);
+  );
+}
 
 module.exports = sequelize;
