9500794ede0de12c9ab6376240444fccd3ce36da
Alberto C. Garcia <albertogarcia767@yahoo.com>
2024-11-21 17:35:18 -0500

    saved

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-<<<<<<< HEAD
 
 let sequelize;
 
@@ -13,25 +12,11 @@ if (process.env.DATABASE_URL) {
       ssl: {
         require: true,
         rejectUnauthorized: false
-=======
-let sequelize;
-console.log(process.env.DB_URL)
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // Helps prevent SSL certificate validation issues
->>>>>>> a225f3df54df04665c615127a826ed8f5bc54485
       }
     }
   });
 } else {
-<<<<<<< HEAD
   // Local database connection
-=======
->>>>>>> a225f3df54df04665c615127a826ed8f5bc54485
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -39,19 +24,12 @@ if (process.env.DB_URL) {
     {
       host: 'localhost',
       dialect: 'postgres',
-<<<<<<< HEAD
       port: 5432,
       logging: false
     }
   );
 }
 
-=======
-      port:'5432'
-    }
-  );
-}
->>>>>>> a225f3df54df04665c615127a826ed8f5bc54485
 module.exports = sequelize;
 // Create connection to database
 
